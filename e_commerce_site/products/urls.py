d2afe619bd6a7ed89430f437749bf3f7aa1bae4a
kejarking <kejarking312@gmail.com>
2021-12-01 17:53:03 +0000

    added get all functionality, crud functionality development

--- e_commerce_site/products/urls.py
+++ e_commerce_site/products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ProductListView
+from .views import ProductDetailView
 
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('<int:pk>/', ProductDetailView.as_view()),
+    path('', ProductListView.as_view()),
 ]
--- e_commerce_site/products/views.py
+++ e_commerce_site/products/views.py
@@ -1,10 +1,27 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
 from .models import Product
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializors import ProductSerializor
 
 # Create your views here.
+
+
+class ProductDetailView(APIView):
+    def get(self, request, pk):
+        prod = Product.objects.get(id=pk)
+        print(prod)
+
+
+class ProductListView(APIView):
+    # GET products
+    def get(self, request):
+        products = Product.objects.all()
+        serialized_products = ProductSerializor(products, many=True)
+        return Response(serialized_products.data, status=status.HTTP_200_OK)
 
 
 def home(request):
